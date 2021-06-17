c7d41c107b9a26f41401df3ede42545bd1189bf4
Aristarkh Zagorodnikov <xm@drive.net>
2021-06-17 22:13:22 +0300

    Remove unnecessary ctor

--- StructuredFieldValues.Tests/TestFailedException.cs
+++ StructuredFieldValues.Tests/TestFailedException.cs
@@ -4,11 +4,6 @@ namespace Xunit.Sdk
 {
     internal sealed class TestFailedException : XunitException
     {
-        public TestFailedException(string userMessage)
-            : base(userMessage)
-        {
-        }
-
         public TestFailedException(string userMessage, Exception innerException)
             : base(userMessage, innerException)
         {
