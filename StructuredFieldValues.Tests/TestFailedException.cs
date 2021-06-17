b34e57f40cb43de7fbddd25ab459c8853088d27a
Aristarkh Zagorodnikov <xm@drive.net>
2021-06-17 20:01:38 +0300

    Allow TestFailedException to have no inner exception

--- StructuredFieldValues.Tests/TestFailedException.cs
+++ StructuredFieldValues.Tests/TestFailedException.cs
@@ -4,6 +4,11 @@ namespace Xunit.Sdk
 {
     internal sealed class TestFailedException : XunitException
     {
+        public TestFailedException(string userMessage)
+            : base(userMessage)
+        {
+        }
+
         public TestFailedException(string userMessage, Exception innerException)
             : base(userMessage, innerException)
         {
