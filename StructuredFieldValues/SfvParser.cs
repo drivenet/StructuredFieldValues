888bc7899c4560ddbd7b933456c8c2e3fec9f0cd
Aristarkh Zagorodnikov <xm@drive.net>
2021-06-18 13:45:32 +0300

    Implement SfvParser API

--- StructuredFieldValues/SfvParser.cs
+++ StructuredFieldValues/SfvParser.cs
@@ -10,22 +10,82 @@ namespace StructuredFieldValues
     /// <remarks>For implementation details check out <see cref="Rfc8941Parser"/>.</remarks>
     public static class SfvParser
     {
-        public static ParseError? ParseItem(ReadOnlySpan<char> source, ref int index, out ParsedItem value)
-            => Rfc8941Parser.ParseItemField(source, ref index, out value);
+        /// <summary>
+        ///     Attempts to parse an item field.
+        /// </summary>
+        ///
+        /// <param name="source">The source string.</param>
+        /// <param name="value">The resulting value.</param>
+        /// <returns>An error if parsing failed; elseway <c>null</c>.</returns>
+        public static ParseError? ParseItem(ReadOnlySpan<char> source, out ParsedItem value)
+        {
+            var index = 0;
+            return Rfc8941Parser.ParseItemField(source, ref index, out value);
+        }
 
-        public static ParseError? ParseItem(string source, ref int index, out ParsedItem value)
-            => Rfc8941Parser.ParseItemField(source, ref index, out value);
+        /// <summary>
+        ///     Attempts to parse an item field.
+        /// </summary>
+        ///
+        /// <param name="source">The source string.</param>
+        /// <param name="value">The resulting value.</param>
+        /// <returns>An error if parsing failed; elseway <c>null</c>.</returns>
+        public static ParseError? ParseItem(string source, out ParsedItem value)
+        {
+            var index = 0;
+            return Rfc8941Parser.ParseItemField(source, ref index, out value);
+        }
 
-        public static ParseError? ParseList(ReadOnlySpan<char> source, ref int index, out IReadOnlyList<ParsedItem> value)
-            => Rfc8941Parser.ParseListField(source, ref index, out value);
+        /// <summary>
+        ///     Attempts to parse an list field.
+        /// </summary>
+        ///
+        /// <param name="source">The source string.</param>
+        /// <param name="value">The resulting value.</param>
+        /// <returns>An error if parsing failed; elseway <c>null</c>.</returns>
+        public static ParseError? ParseList(ReadOnlySpan<char> source, out IReadOnlyList<ParsedItem> value)
+        {
+            var index = 0;
+            return Rfc8941Parser.ParseListField(source, ref index, out value);
+        }
 
-        public static ParseError? ParseList(string source, ref int index, out IReadOnlyList<ParsedItem> value)
-            => Rfc8941Parser.ParseListField(source, ref index, out value);
+        /// <summary>
+        ///     Attempts to parse a list field.
+        /// </summary>
+        ///
+        /// <param name="source">The source string.</param>
+        /// <param name="value">The resulting value.</param>
+        /// <returns>An error if parsing failed; elseway <c>null</c>.</returns>
+        public static ParseError? ParseList(string source, out IReadOnlyList<ParsedItem> value)
+        {
+            var index = 0;
+            return Rfc8941Parser.ParseListField(source, ref index, out value);
+        }
 
-        public static ParseError? ParseDictionary(ReadOnlySpan<char> source, ref int index, out IReadOnlyDictionary<string, ParsedItem> value)
-            => Rfc8941Parser.ParseDictionaryField(source, ref index, out value);
+        /// <summary>
+        ///     Attempts to parse a dictionary field.
+        /// </summary>
+        ///
+        /// <param name="source">The source string.</param>
+        /// <param name="value">The resulting value.</param>
+        /// <returns>An error if parsing failed; elseway <c>null</c>.</returns>
+        public static ParseError? ParseDictionary(ReadOnlySpan<char> source, out IReadOnlyDictionary<string, ParsedItem> value)
+        {
+            var index = 0;
+            return Rfc8941Parser.ParseDictionaryField(source, ref index, out value);
+        }
 
-        public static ParseError? ParseDictionary(string source, ref int index, out IReadOnlyDictionary<string, ParsedItem> value)
-            => Rfc8941Parser.ParseDictionaryField(source, ref index, out value);
+        /// <summary>
+        ///     Attempts to parse a dictionary field.
+        /// </summary>
+        ///
+        /// <param name="source">The source string.</param>
+        /// <param name="value">The resulting value.</param>
+        /// <returns>An error if parsing failed; elseway <c>null</c>.</returns>
+        public static ParseError? ParseDictionary(string source, out IReadOnlyDictionary<string, ParsedItem> value)
+        {
+            var index = 0;
+            return Rfc8941Parser.ParseDictionaryField(source, ref index, out value);
+        }
     }
 }
