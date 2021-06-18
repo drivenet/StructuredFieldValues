41227d95c0c60fb9a7d8125dad8750529dd4d669
Aristarkh Zagorodnikov <xm@drive.net>
2021-06-18 13:14:53 +0300

    Add SfvParser

---
+++ StructuredFieldValues/SfvParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StructuredFieldValues
+{
+    public static class SfvParser
+    {
+        public static ParseError? Parse(ReadOnlySpan<char> source, FieldType fieldType, ref int index, out object result)
+            => Rfc8941Parser.ParseField(source, fieldType, ref index, out result);
+
+        public static ParseError? Parse(string source, FieldType fieldType, ref int index, out object result)
+            => Rfc8941Parser.ParseField(source, fieldType, ref index, out result);
+    }
+}
